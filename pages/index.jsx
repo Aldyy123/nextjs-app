858fcf942d22cf9a43880d80ff03106b88d0d49a
Aldyy123 <bocah.ngik@gmail.com>
2022-05-13 05:49:25 +0700

    change commit

---
+++ pages/index.jsx
@@ -0,0 +1,24 @@
+import Head from "next/head"
+
+export default function About() {
+    return (
+        <>
+        <Head>
+            <title>About Me</title>
+            <meta name="description" content="M. Ardi Trisnaldi - About Me" />
+            <meta name="og:description" content="M. Ardi Trisnaldi - About Me" />
+            <meta name="og:title" content="About me - M. Ardi Trisnaldi" />
+            <meta name="og:type" content="website" />
+        </Head>
+            <p>Hai 👋 selamat datang.</p>
+            <p>
+                Saya seorang freelance developer yang berfokus ke dalam website frontend maupung backend.
+                Awal mula saya menjadi programmer itu karena saya suka mempelajari hal yang berbau IT Sejak SMP,
+                dimana mempelajari hardware komputer, jaringan dan design. Namun saya bertemu dengan hal yang benar - benar saya ingin fokus
+                dan saya sukai yaitu Developer. Menjadi developer seperti hal yang magic bagi saya, melakukan hal yang mustahil menjadi tidak mustahil.
+                Saya mempelajari bahasa pemprogramman sejak tahun 2019 sampai sekarang.  Stack yang saya gunakan itu javascript, css dan php.
+                Framework yang saya gunakan pada saat membuat website itu react, karena framework yang mudah dan modern untuk website jaman now
+            </p>
+        </>
+    )
+}
