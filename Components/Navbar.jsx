62ab656b98ebd6f09d839759f6e42511a5a372e2
Aldyy231 <53711786+Aldyy123@users.noreply.github.com>
2022-08-21 03:49:15 +0700

    memo optimization

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -2,13 +2,16 @@ import styles from '../styles/Navbar.module.css'
 import { useRouter } from "next/router";
 import { useSpring, animated } from 'react-spring'
 import Navlink from "./Navlink";
+import { useMemo } from 'react';
 
 export default function Navbar() {
-    const router = useRouter()
+    const routerNext = useRouter()
 
     const props = useSpring({
         to: { opacity: 1, translateX: '0' }, from: { opacity: 0, translateX: '-40px' }, delay: 400,
     })
+
+    const router = useMemo(() => routerNext, [routerNext])
 
     return (
         <>
@@ -17,9 +20,9 @@ export default function Navbar() {
                 <animated.hr style={props} />
             </div>
             <section className={styles.section}>
-                <Navlink href={'/'} title="About me" />
-                <Navlink href={'/project'} title="Project" />
-                <Navlink href={'/contact'} title="Contact" />
+                <Navlink href={'/'} title="About me" pathname={router.pathname} />
+                <Navlink href={'/project'} title="Project" pathname={router.pathname} />
+                <Navlink href={'/contact'} title="Contact" pathname={router.pathname} />
             </section>
         </>
     )
--- Components/Navlink.jsx
+++ Components/Navlink.jsx
@@ -1,12 +1,10 @@
 import Link from "next/link"
 import { memo, useCallback, useState, useMemo } from "react"
 import { Spring, animated } from "react-spring"
-import { useRouter } from "next/router";
 import styles from '../styles/Navbar.module.css'
 
-function Navlink({ href, title }) {
+function Navlink({ href, title, pathname }) {
     const [isHover, setIsHover] = useState(false)
-    const router = useRouter()
     const HoverEnter = useCallback(() => {
         setIsHover(true)
     }, [setIsHover])
@@ -14,8 +12,6 @@ function Navlink({ href, title }) {
     const HoverLeave = useCallback(() => {
         setIsHover(false)
     }, [setIsHover])
-    
-    const props = useMemo(() => { return { router, href, title } }, [router, title, href])
     return (
         <>
             <li className={`poppins`}
@@ -23,13 +19,13 @@ function Navlink({ href, title }) {
                 onMouseLeave={() => HoverLeave()}
             >
                 <Link href={href} passHref>
-                    <a className={props.router.pathname === props.href ? styles.active : ''}>
+                    <a className={pathname === href ? styles.active : ''}>
                         {title}
                     </a>
                 </Link>
                 <Spring to={{
-                    opacity: isHover || props.router.pathname === props.href ? 1 : 0,
-                    translateX: isHover || props.router.pathname === props.href ? '0px' : '-40px',
+                    opacity: isHover || pathname === href ? 1 : 0,
+                    translateX: isHover || pathname === href ? '0px' : '-40px',
                 }}>
                     {stylesSpring => (
                         <animated.hr style={{ ...stylesSpring, height: '3px!important', backgroundColor: '#371B58', width: '100px',}} />
