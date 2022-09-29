f6dc172442f4870765cacb8ee15640e72b13cee7
Aldyy231 <53711786+Aldyy123@users.noreply.github.com>
2022-09-29 23:15:48 +0700

    Add data json portfolio, cv, and revision about

--- pages/index.jsx
+++ pages/index.jsx
@@ -33,13 +33,14 @@ export default function About() {
                 <hr style={styles.hr} />
                 <p>Hai 👋 selamat datang.</p>
                 <p>
-                    Saya seorang freelance developer yang berfokus ke dalam website frontend maupung backend.
+                    Saya seorang freelance developer yang berfokus ke dalam website frontend maupun backend.
                     Awal mula saya menjadi programmer itu karena saya suka mempelajari hal yang berbau IT Sejak SMP,
                     dimana mempelajari hardware komputer, jaringan dan design. Namun saya bertemu dengan hal yang benar - benar saya ingin fokus
                     dan saya sukai yaitu Developer. Menjadi developer seperti hal yang magic bagi saya, melakukan hal yang mustahil menjadi tidak mustahil.
                     Saya mempelajari bahasa pemprogramman sejak tahun 2019 sampai sekarang.  Stack yang saya gunakan itu javascript, css dan php.
                     Framework yang saya gunakan pada saat membuat website itu react, karena framework yang mudah dan modern untuk website jaman now
                 </p>
+                <a className="see-cv" target={'_blank'} href="https://drive.google.com/file/d/1uP4aTGrmyjDNVYvRNYZ57BaJD1M2xxLV/view?usp=sharing" rel="noreferrer">📋 See CV</a>
             </animated.div>
         </>
     )
