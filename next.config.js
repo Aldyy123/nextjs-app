771b795e8c2bef5f33a5ff1ded0254cf330b19cc
Aldyy123 <bocah.ngik@gmail.com>
2023-04-23 19:34:48 +0700

    change output

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'build',
   images: {
     domains: ["cdn.pixabay.com", 'img.icons8.com', 'raw.githubusercontent.com']
   },
