c1754a7c143cdcf3b5374b57b478d1b18c34bf05
Aldyy123 <mohammad.aldyy@gmail.com>
2023-08-13 18:57:09 +0700

    google search console

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import Layout from "../Components/Layout";
 import Script from "next/script";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Layout>
+        <Head>
+          <meta name="google-site-verification" content="ybVSCkjkC6Bd8S0tyITXrOaTjHO95Ex7BCWb5nXAIYo" />
+        </Head>
         <Script
           strategy="lazyOnload"
           src="https://www.googletagmanager.com/gtag/js?id=G-1M8T6WVH03"
